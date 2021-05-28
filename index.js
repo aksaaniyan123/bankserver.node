46704d5c6998cab551b845225a6b17624b762d8d
unknown <aksakaniyan96@gmail.com>
2021-05-28 10:59:15 +0530

    middleware and session

--- index.js
+++ index.js
@@ -1,36 +1,107 @@
+const express=require('express');
+const session=require('express-session');
+const dataService=require('./service/data.service')
+const app=express();
 
-const express = require('express');//import statement ....require means import
-const dataService=require('./service/data.service');
-const app = express();//oru application crete cheyyan express call cheythu
-app.use(express.json())
+
+app.use(session({//to create session
+    secret: 'randomsecurestring',
+    resave:false,
+    saveUninitialized:false
+}));
 
 
-app.get('/',(req,res)=>{// '/' means empty path...request,respone
-    res.status(401).send("this is a get method")
+app.use(express.json());
+
+
+app.use((req,res,next)=>{//to creeate middleware
+console.log("Middleware");
+// if(!req.session.currentUser){
+//     // return {
+//     //   statusCode:401,
+//     //   status:false,
+//     //   message:"plz login"
+//     // }
+//     console.log("Middleware");
+//   }
+//   else{
+//     next();
+//   }
+next();
+
+})
+const logMiddleware=(req,res,next)=>{
+    console.log(req.body);
+    next();
+}
+app.use(logMiddleware);
+
+
+
+const authMiddleware=(req,res,next)=>{
+    if(!req.session.currentUser){
+             return res.json({
+              statusCode:401,
+               status:false,
+               message:"plz login"
+             })
+            
+          }
+else{
+    next();
+}
+}
+
+app.get('/',(req,res)=>{
+    res.status(401).send("This is a Get method")
+
 });
 app.post('/',(req,res)=>{
     res.send("this is a post method")
+
 });
 app.post('/register',(req,res)=>{
-   const result= dataService.register(req.body.uname,req.body.acno,req.body.pswd);
-   console.log (res.send(result.message));
+
+    const result=dataService.register(req.body.uname,req.body.acno,req.body.pswd);;
+    res.status(result.statusCode).json(result)
+   
 });
 app.post('/login',(req,res)=>{
-    const result= dataService.login(req.body.acno,req.body.pswd);
-    res.status(result.statuscode).json(result)
-    //console.log (res.send(result.message));
- });
+
+    const result=dataService.login(req,req.body.acno,req.body.pswd);;
+    res.status(result.statusCode).json(result)
+   
+});
+
+
+app.post('/deposit',authMiddleware,(req,res)=>{
+    console.log(req.session.currentUser);
+   
+
+    const result=dataService.deposit(req.body.acno,req.body.pswd,req.body.amount);
+    res.status(result.statusCode).json(result)
+});
+
+
+app.post('/withdraw',authMiddleware,(req,res)=>{
+ 
+    const result=dataService.withdraw(req.body.acno,req.body.pswd,req.body.amount);
+    res.status(result.statusCode).json(result)
+});
+
+app.put('/',(req,res)=>{
+    res.send("this is a put method")
+
+})
 app.patch('/',(req,res)=>{
     res.send("this is a patch method")
+
 });
 app.delete('/',(req,res)=>{
     res.send("this is a delete method")
+
 });
-app.listen(3000,()=>{//call back function//port set cheythu
-    console.log("server started at port 3000");
-})
+app.listen(3000,()=>{
+    console.log("Server Started at port:3000");
 
-
-
-
-
+});
--- service/data.service.js
+++ service/data.service.js
@@ -1,64 +1,127 @@
-accountDetails = {
-    1000: { acno: 1000, username: "userone", password: "userone", balance: 50000 },
+let currentUser;
+let accountDetails= {
+    1000: { acno: 1000,  username: "userone", password: "userone", balance: 50000 },
     1001: { acno: 1001, username: "usertwo", password: "usertwo", balance: 5000 },
-    1002: { acno: 1002, username: "userthree", password: "userthree", balance: 10000 },
-    1003: { acno: 1003, username: "userfour", password: "userfour", balance: 6000 }
-}
-const register = (uname, acno, pswd) => {
-    let user = accountDetails;
-    if (acno in user) {
-        return {
-            statuscode:422,
-            status: false,
-            message: "user exist plz login"
-        }
-    }
-    else {
-        user[acno] = {
-            acno,
-            username: uname,
-            password: pswd,
-            balance: 0
-        }
+    1002: { acno: 1002,  username: "userthree", password: "userthree", balance: 10000 },
+    1003: { acno: 1003,  username: "userfour", password: "userfour", balance: 6000 }
+  }
+  const register=(uname,acno,pswd)=>{
 
-        return {
-            statuscode:200,
-            status: true,
-            message: "successfully registered"
-        }
-    }
-}
-const login = (acno, pswd) => {
-    let users = accountDetails;
-    if (acno in users) {
-        if (pswd == users[acno]["password"]) {
-            currentUser = users[acno]["username"]
-
-            return {
-                statuscode: 200,
-                status: true,
-                message: "sucessfully login"
-            }
-        }
-
-        else {
-            return {
-                statuscode: 422,
-                status: false,
-                message: "invalid password"
-            }
-        }
-    }
-
-    else {
+    let user=accountDetails;
+    if(acno in user){
         return{
-        statusCode: 422,
-            status: false,
-                message: "invalid account"
+            statusCode:422,
+            status:false,
+            message:"User exists,please login"
+        }
+
+    }
+    else{
+        user[acno]={
+            acno,
+            username:uname,
+            password:pswd,
+            balance:0
+        }
+        return{
+            statusCode:200,
+            status:true,
+            message:"Successfully reigsterd"
+        }
+    }
+  }
+  const login=(req,acno,pswd)=>{
+    let user=accountDetails;
+    if (acno in user) {
+  
+      if(pswd == user[acno]["password"]){
+       req.session. currentUser=user[acno]
+        return {
+            statusCode:200,
+            status:true,
+            message:"Succesfuly login"
+        }
+       
+      }
+    
+      else{
+       return {
+        statusCode:422,
+        status:false,
+        message:"invld pswd"
+       }
+      }
+    }
+    else{
+      return {
+        statusCode:422,
+        status:false,
+        message:"invalid acnt"
+      }
     }
 }
+const deposit=(acno,pswd,amt)=> {
+  
+
+  var amount=parseInt(amt)
+  let user=accountDetails;
+  if (acno in user){
+    if(pswd == user[acno]["password"]){
+      user[acno]["balance"]+=amount;
+      return{
+        statusCode:200,
+        status:true,
+        balance:user[acno]["balance"],
+        message:amount+"crdtd and new blnce is " +user[acno]["balance"]
+    }
+      }
+      else{
+        return {
+          statusCode:422,
+          status:false,
+          message:"incrct pswd"
+        }
+      }}
+
+else{
+  return{
+statusCode:422,
+status:false,
+message:"invalid acnt"
 }
-module.exports = {
-    register,
-    login
+}}
+const withdraw=(acno,pswd,amt)=> {
+  var amount=parseInt(amt)
+  let user=accountDetails;
+  if (acno in user){
+    if(pswd == user[acno]["password"]){
+      user[acno]["balance"]-=amount;
+      return{
+        statusCode:200,
+        status:true,
+        balance:user[acno]["balance"],
+        message:amount+"debited and new blnce is " +user[acno]["balance"]
+    }
+      }
+      else{
+        return {
+          statusCode:422,
+          status:false,
+          message:"incrct pswd"
+        }
+      }}
+
+else{
+  return{
+statusCode:422,
+status:false,
+message:"invalid acnt"
 }
+}}
+
+module.exports={
+      register,
+      login,
+      deposit,
+      withdraw
+  }
